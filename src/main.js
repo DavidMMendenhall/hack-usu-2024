e8d9b46e745cec59ec020b3f8946aa45d8caa8fe
David Mendenhall <davidmendenhallact4333@gmail.com>
2024-03-02 15:17:26 -0700

    corrected textures

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ window.onConnection = function() {
         el.style.visibility = "visible";
     }
     fetch("./assets/levels/eyeglass-lake.json")
+    // fetch("./assets/levels/twirl-island.json")
+    // fetch("./assets/levels/five-rocks.json")
     .then(data => data.json())
     .then(j => Game.loadLevel(j));
 }
